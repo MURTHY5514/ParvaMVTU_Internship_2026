94860643b56601f01521369ad271571ff9acb56b
MURTHY5514 <murthygovindraj123@email.com>
2026-04-29 12:58:47 +0530

    upadted code

---
+++ java_springboot/SpringSecurity2/src/main/java/com/springsecurity2/config/UserConfig.java
@@ -0,0 +1,33 @@
+package com.springsecurity2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserConfig {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return  new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService () {
+		UserDetails user1 = User.withUsername("pavan")
+				.password(passwordEncoder()
+						.encode("Pavan@123"))
+				.roles("ADMIN").build();
+		UserDetails user2 = User.withUsername("darshan")
+				.password(passwordEncoder()
+						.encode("Darshan@123"))
+				.roles("ADMIN").build();
+		return new InMemoryUserDetailsManager(user1, user2);
+	}
+	
+}
